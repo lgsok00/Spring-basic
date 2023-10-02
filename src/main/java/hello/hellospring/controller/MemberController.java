fbcb948b06c97edeffc7db48d90050c5a422c93d
lgsok00 <lgsok00@github.com>
2023-10-02 18:46:05 +0900

    회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // @Controller 애노테이션이 붙으면 Spring 컨테이너가 스프링 객체를 생성
 public class MemberController {
@@ -29,5 +32,12 @@ public class MemberController {
     memberService.join(member);
 
     return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
